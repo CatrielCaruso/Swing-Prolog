4e6e9be552162564a99b063cfcca34c19c4161ea
CatrielCaruso <catrielcaruso@gmail.com>
2019-05-17 10:46:11 -0300

    Add files via upload

---
+++ Practica1/Ejercicio9.pl
@@ -0,0 +1,10 @@
+hijo(juan,miguel).
+hijo(jose,miguel).
+hijo(miguel,roberto).
+hijo(julio,roberto).
+hijo(roberto,carlos).
+
+descendiente(A,B):- hijo(A,B).
+descendiente(A,B):- hijo(A,Z),descendiente(Z,B).
+
+
